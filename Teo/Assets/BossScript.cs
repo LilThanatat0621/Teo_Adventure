fc8386048b4c52d2f744635a715cece9fdd99b12
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-02-04 23:05:15 +0700

    add stage sample2

---
+++ Teo/Assets/BossScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour {
+    // Start is called before the first frame update
+    public float jumpSpeed = 9f;
+    public float jumpPower = 20f;
+    private Rigidbody2D rigidbody2d;
+    void Start () {
+        rigidbody2d = gameObject.GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void Jump () {
+        rigidbody2d.AddForce ((Vector2.up * jumpPower) * jumpSpeed);
+    }
+
+}
