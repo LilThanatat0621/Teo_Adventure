1423a3665c6c9ebddb5b5b1f85fef66879f9ce93
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-02-08 00:16:31 +0700

    add boss script

--- Teo/Assets/BossScript.cs
+++ Teo/Assets/BossScript.cs
@@ -10,24 +10,52 @@ public class BossScript : MonoBehaviour {
     public GameObject HitArea;
     public float jumpPower = 20f;
     private Rigidbody2D rigidbody2d;
+    public int live = 5;
+    bool isDie = false;
     void Start () {
         rigidbody2d = gameObject.GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update () {
-        if (temp >= fireRate) {
+        if (temp >= fireRate && !isDie) {
             temp = 0;
             Instantiate (HitArea, new Vector3 (transform.position.x - 1.5f, transform.position.y + 0.25f, transform.position.z), Quaternion.Euler (0, 180, 0));
         }
+        if (live <= 0) {
+            if (!isDie) Die ();
+            isDie = true;
+        }
+        
     }
-
+    void Die () {
+        rigidbody2d.AddForce ((Vector2.up * jumpPower) * jumpSpeed * 3);
+        this.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+    }
     public void Fire () {
         temp++;
 
     }
     public void Jump () {
-        rigidbody2d.AddForce ((Vector2.up * jumpPower) * jumpSpeed);
+        if (!isDie) rigidbody2d.AddForce ((Vector2.up * jumpPower) * jumpSpeed);
+    }
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "bullet") {
+            live--;
+            this.gameObject.GetComponent<SpriteRenderer>().color=Color.red;
+            StartCoroutine (WaitRestore ());
+        }
+    }
+
+    IEnumerator WaitRestore () {
+		yield return new WaitForSeconds (0.1f);
+		this.gameObject.GetComponent<SpriteRenderer>().color=Color.white;
+	}
+    void OnTriggerExit2D (Collider2D other) {
+        if (other.gameObject.name == "danger") {
+            Destroy (this.gameObject);
+        }
     }
 
 }
