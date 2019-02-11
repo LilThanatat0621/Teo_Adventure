3ea68b321870898c572ab9d4161805898519fedb
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-02-11 18:35:07 +0700

    Add panel to textPos

--- Teo/Assets/Rec.cs
+++ Teo/Assets/Rec.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class Rec : MonoBehaviour {
     // Start is called before the first frame update
     // private coordinatesX,coordinatesY;
+    RectTransform rect;
     void Start () {
         this.transform.localPosition = new Vector3 (0, 0, 0);
-        // this.transform.localScale=new Vector3(this.transform.localScale.x*this.transform.parent.localScale.x,this.transform.localScale.y*this.transform.parent.localScale.y,this.transform.localScale.z*this.transform.parent.localScale.z);
+        rect=this.gameObject.GetComponent<RectTransform>();
+        
     }
 
     // Update is called once per frame
     void Update () {
         this.transform.localPosition = new Vector3 (0, 0, 0);
-        // Debug.Log(transform.position);
+        rect.sizeDelta=new Vector2(GetWidht(this.transform.parent.gameObject.GetComponent<TextMesh>())+2.5f,rect.sizeDelta.y);
+        this.gameObject.GetComponent<SpriteRenderer>().size=rect.sizeDelta;
     }
+
+     public static float GetWidht(TextMesh mesh)
+ {
+     float width = 0;
+     foreach( char symbol in mesh.text)
+     {
+         CharacterInfo info;
+         if (mesh.font.GetCharacterInfo(symbol, out info))
+         {
+             width += info.width;
+         }
+     }
+     return width * mesh.characterSize * 0.1f   ;
+ }
 }
