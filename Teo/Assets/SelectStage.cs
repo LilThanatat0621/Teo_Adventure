41caa33d5cf2694efafe7f59e7503e8be802b6a7
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-02-13 07:05:13 +0700

    add sound

--- Teo/Assets/SelectStage.cs
+++ Teo/Assets/SelectStage.cs
@@ -2,40 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class SelectStage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-{
+using UnityEngine.UI;
+public class SelectStage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     // Start is called before the first frame update
     public string stage;
     public Text child;
     Color temp;
-    void Start()
-    {
+
+    private AudioSource aSource;
+    void Start () {
+        DestroyAllDontDestroyOnLoadObjects ();
         temp = child.color;
+        aSource = gameObject.AddComponent<AudioSource> ();
+        aSource.clip = Resources.Load<AudioClip> ("Sound/command");
     }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
+    public void DestroyAllDontDestroyOnLoadObjects () {
+
+        var go = new GameObject ("Sacrificial Lamb");
+        DontDestroyOnLoad (go);
+
+        foreach (var root in go.scene.GetRootGameObjects ())
+            Destroy (root);
+
+    }
+    public void OnPointerEnter (PointerEventData eventData) {
         // GetComponent<RectTransform>().sizeDelta = new Vector2(160f, 54);
         // child.fontSize = 48;
+        if (!aSource.isPlaying) {
+            aSource.Play ();
+        }
         child.color = Color.white;
     }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
+    public void OnPointerExit (PointerEventData eventData) {
         // GetComponent<RectTransform>().sizeDelta = new Vector2(145f, 54);
         // child.fontSize = 36;
         child.color = temp;
     }
 
-
-    public void Press()
-    {
-        SceneManager.LoadScene(stage);
+    public void Press () {
+        SceneManager.LoadScene (stage);
     }
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
 
     }
 }
